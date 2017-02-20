101d2a2e00c9ba8422d03cc12f1ef3e9053e9a3e
joshinachiket <nachiket.r.joshi@gmail.com>
2017-02-20 00:19:10 -0800

    Game-Of-Life-Spawn-Commit

---
+++ Game-Of-Life/app/src/main/java/com/joshi/nachiket/game_of_life/MainActivity.java
@@ -0,0 +1,33 @@
+package com.joshi.nachiket.game_of_life;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public Button start_button;
+
+    public void startButton () {
+        start_button = (Button)findViewById(R.id.button);
+        start_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent purpose = new Intent(MainActivity.this, GameLife.class);
+                startActivity(purpose);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton();
+    }
+}
